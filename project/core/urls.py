256668965fb6aa53bfb9615372c72eba293a8d85
tyransoft <tyransoftagency@gmail.com>
2026-09-11 16:20:30 +0200

    5

--- project/core/urls.py
+++ project/core/urls.py
@@ -6,5 +6,23 @@ from django.contrib.sitemaps.views import sitemap
 urlpatterns = [
 
     path('', home, name='home'),
-  
+
+    path('products/', products, name='products'),
+    path('products/baby-diapers/',pampers,name='pampers'),
+    path('products/wipes/',wipes,name='wipes'),
+    path('products/personal-care/',mothers,name='mothers'),
+
+    path('about/',about,name='about'),
+
+    path('quality/',quality,name='quality'),
+    path('why-us/',why_us,name='why_us'),
+    path('our-story/',our_story,name='ourstory'),
+    path('connect-us/',connect_us,name='connectus'),
+
+
+
+
+
+
+
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -3,3 +3,34 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def products(request):
+    return render(request , 'products.html')
+
+
+def wipes(request):
+    return render(request , 'wipes.html')
+
+
+def pampers(request):
+    return render(request , 'pampers.html')
+
+
+def mothers(request):
+    return render(request , 'mothers.html')
+
+
+def about(request):
+    return render(request , 'about.html')
+
+def connect_us(request):
+    return render(request, 'connect.html')
+
+def our_story(request):
+    return render(request, 'our_story.html')
+
+def quality(request):
+    return render(request, 'quality.html')
+
+def why_us(request):
+    return render(request, 'why_us.html')  
